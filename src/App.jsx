42c3e7191434ae6a1b01b7d9e5976aec18921745
nguyenphujj <nguyenxuanphujj@gmail.com>
2025-08-17 12:19:27 +0700

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -462,7 +462,7 @@ function Another({outpassBackpage, outpassjson, inpassServer}){
         onChange={(e) => setInputcache(e.target.value)}
         placeholder="your message"
       />
-      <button onClick={handleSend}>sendx</button>
+      <button onClick={handleSend}>sendx2</button>
     </div>
   );
 }
